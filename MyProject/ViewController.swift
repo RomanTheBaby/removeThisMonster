58c343825fe7ffb2f42aefd707b2f66d1f50a7eb
RomanTheBaby <mailofrom@gmail.com>
2018-04-04 20:31:49 +0300

    Initial Commit

---
+++ MyProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyProject
+//
+//  Created by Baby on 4/4/18.
+//  Copyright © 2018 Alina. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
